7bfca66bb172a3765d83b25c5abf6c6ad16ab334
msciborski <michal.sciborski@student.put.poznan.pl>
2018-03-17 18:13:06 +0100

    Adding project to sln (BookMe2MObi.MobiMetadata), Adding util class for working with bytes (Converting).

---
+++ BookMeMobi2.MobiMetadata/Utilities/ByteUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookMeMobi2.MobiMetadata.Utilities
+{
+    internal static class ByteUtils
+    {
+        public static short ToInt16(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        public static int ToInt32(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public static int GetInt32(byte[] bytes)
+        {
+            var result = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                result = (result << 8) + (bytes[i] & 0xff);
+            }
+
+            return result;
+        }
+
+        public static long ToUInt32(byte[] bytes)
+        {
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(bytes);
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        public static string ToString(byte[] bytes)
+        {
+            if (bytes == null)
+                return "";
+            return Encoding.ASCII.GetString(bytes).Replace("\0", "");
+        }
+    }
+}
+}
