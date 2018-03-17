1d874763354c23435d39a9729fdb11e9f955f126
msciborski <michal.sciborski@student.put.poznan.pl>
2018-03-17 16:35:07 +0100

    Adding mapping profile, Creating controller method GetAllUsers, Creating Dto UserDto, and changing name UserDto to UserRegisterDto

--- BookMeMobi2/Models/UserRegisterDto.cs
+++ BookMeMobi2/Models/UserRegisterDto.cs
@@ -5,12 +5,13 @@ using System.Threading.Tasks;
 
 namespace BookMeMobi2.Models
 {
-    public class UserDto
+    public class UserRegisterDto
     {
         public string UserName { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Location { get; set; }
+        public string Password { get; set; }
     }
 }
