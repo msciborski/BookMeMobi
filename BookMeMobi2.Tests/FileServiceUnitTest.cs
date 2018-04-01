1fe36c75bfcae6b56c7f44b63f342f287a0d9245
msciborski <michal.sciborski@student.put.poznan.pl>
2018-04-01 09:24:13 +0200

    Add tes fixture for file service.

--- BookMeMobi2.Tests/FileServiceUnitTest.cs
+++ BookMeMobi2.Tests/FileServiceUnitTest.cs
@@ -21,156 +21,92 @@ namespace BookMeMobi2.Tests
 {
     public class FileServiceUnitTest
     {
+
         #region GetBookForUserAsync
+
         [Fact]
         public async Task ReturnBookForUserIdAndBookId()
         {
-            var options = SqliteInMemory.CreateOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
+            using (var fixture = new FileServiceTestFixture())
             {
                 //Arrange
-                context.Database.EnsureCreated();
-                await context.SeedDatabaseWithUsersAndBooks();
-
-                var mock = new Mock<ILogger<FileService>>();
-                ILogger<FileService> logger = mock.Object;
-
-                var mapperMock = new Mock<IMapper>();
-                var mapper = mapperMock.Object;
-
-                IOptions<GoogleCloudStorageSettings> googleOptions =
-                    Microsoft.Extensions.Options.Options.Create(new GoogleCloudStorageSettings());
-
-                IFileService fileService = new FileService(googleOptions, mapper, context, logger);
-
+                await fixture.SeedDatabase(); ;
                 //Action
-                var bookDto = await fileService.GetBookForUserAsync("ID1", 1);
+                var bookDto = await fixture.FileService.GetBookForUserAsync("ID1", 1);
 
                 //Assert
                 bookDto.Id.ShouldEqual(1);
                 bookDto.DeleteDate.ShouldBeNull();
                 bookDto.UserId.ShouldEqual("ID1");
             }
+
         }
 
         [Fact]
         public async Task ThrowBookNoFoundExceptionForInvalidBookId()
         {
-            var options = SqliteInMemory.CreateOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
+            using (var fixture = new FileServiceTestFixture())
             {
                 //Arrange
-                context.Database.EnsureCreated();
-                await context.SeedDatabaseWithUsersAndBooks();
+                await fixture.SeedDatabase();
 
-                var mock = new Mock<ILogger<FileService>>();
-                ILogger<FileService> logger = mock.Object;
-
-                var mapperMock = new Mock<IMapper>();
-                var mapper = mapperMock.Object;
-
-                IOptions<GoogleCloudStorageSettings> googleOptions =
-                    Microsoft.Extensions.Options.Options.Create(new GoogleCloudStorageSettings());
-
-                IFileService fileService = new FileService(googleOptions, mapper, context, logger);
-
-                //Action
+                //Action&Assert
                 await Assert.ThrowsAsync<BookNoFoundException>(async () =>
-                    await fileService.GetBookForUserAsync("ID1", 11111));
-
+                    await fixture.FileService.GetBookForUserAsync("ID1", 11111));
             }
         }
 
         [Fact]
         public async Task ThrowUserNoFoundExceptionForInvalidUserId()
         {
-            var options = SqliteInMemory.CreateOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
+            using (var fixture = new FileServiceTestFixture())
             {
                 //Arrange
-                context.Database.EnsureCreated();
-                await context.SeedDatabaseWithUsersAndBooks();
+                await fixture.SeedDatabase();
 
-                var mock = new Mock<ILogger<FileService>>();
-                ILogger<FileService> logger = mock.Object;
-
-                var mapperMock = new Mock<IMapper>();
-                var mapper = mapperMock.Object;
-
-                IOptions<GoogleCloudStorageSettings> googleOptions =
-                    Microsoft.Extensions.Options.Options.Create(new GoogleCloudStorageSettings());
-
-                IFileService fileService = new FileService(googleOptions, mapper, context, logger);
-
-                //Action
+                //Action&Assert
                 await Assert.ThrowsAsync<UserNoFoundException>(async () =>
-                    await fileService.GetBookForUserAsync("ID1000", 1));
-
+                    await fixture.FileService.GetBookForUserAsync("ID1000", 1));
             }
+
+
         }
-
-
         #endregion
 
         #region GetBooksForUserAsync
+
         [Fact]
         public async Task ReturnBooksForUserId()
         {
-            var options = SqliteInMemory.CreateOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
+            using (var fixture = new FileServiceTestFixture())
             {
                 //Arrange
-                context.Database.EnsureCreated();
-                await context.SeedDatabaseWithUsersAndBooks();
-
-                var mock = new Mock<ILogger<FileService>>();
-                ILogger<FileService> logger = mock.Object;
-                
-                var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfile>());
-                var mapper = config.CreateMapper();
-
-                IOptions<GoogleCloudStorageSettings> googleOptions =
-                    Microsoft.Extensions.Options.Options.Create(new GoogleCloudStorageSettings());
-
-                IFileService fileService = new FileService(googleOptions, mapper, context, logger);
-
-                //Action
+                await fixture.SeedDatabase();
+                //Action&Assert
 
                 await Assert.ThrowsAsync<UserNoFoundException>(async () =>
-                    await fileService.GetBooksForUserAsync("ID101", 10, 1));
-
+                    await fixture.FileService.GetBooksForUserAsync("ID101", 10, 1));
             }
+
+
         }
 
         [Fact]
         public async Task GetBooksForUserIdInvalidUserId()
         {
-            var options = SqliteInMemory.CreateOptions<ApplicationDbContext>();
-            using (var context = new ApplicationDbContext(options))
+            using (var fixture = new FileServiceTestFixture())
             {
                 //Arrange
-                context.Database.EnsureCreated();
-                await context.SeedDatabaseWithUsersAndBooks();
-
-                var mock = new Mock<ILogger<FileService>>();
-                ILogger<FileService> logger = mock.Object;
-
-                var config = new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperProfile>());
-                var mapper = config.CreateMapper();
-
-                IOptions<GoogleCloudStorageSettings> googleOptions =
-                    Microsoft.Extensions.Options.Options.Create(new GoogleCloudStorageSettings());
-
-                IFileService fileService = new FileService(googleOptions, mapper, context, logger);
-
+                await fixture.SeedDatabase();
                 //Action
 
-                var result = await fileService.GetBooksForUserAsync("ID1", 10, 1);
+                var result = await fixture.FileService.GetBooksForUserAsync("ID1", 10, 1);
+
+                //Assert
 
                 result.Items.Count.ShouldEqual(2);
                 result.HasNextPage.ShouldEqual(false);
                 result.PageSize.ShouldEqual(10);
-
             }
         }
 
@@ -178,9 +114,18 @@ namespace BookMeMobi2.Tests
 
         #region DeleteBook(soft)
 
+        public async Task DeleteBookForValidUserIdAndBookId()
+        {
+            using (var fixture = new FileServiceTestFixture())
+            {
 
+            }
+        }
 
         #endregion
+
+
     }
 }
+
 
