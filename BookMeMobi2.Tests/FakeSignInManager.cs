0a7a8b68838e29d93d868c0014c92085656cb438
msciborski <michal.sciborski@student.put.poznan.pl>
2018-04-02 13:30:13 +0200

    Creating FakeSignInManager and TextFixture for UserServiceTest

---
+++ BookMeMobi2.Tests/FakeSignInManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookMeMobi2.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace BookMeMobi2.Tests
+{
+    public class FakeSignInManager : SignInManager<User>
+    {
+        public FakeSignInManager()
+            : base(new Mock<FakeUserManager>().Object,
+                   new HttpContextAccessor(),
+                   new Mock<IUserClaimsPrincipalFactory<User>>().Object,
+                   new Mock<IOptions<IdentityOptions>>().Object,
+                   new Mock<ILogger<SignInManager<User>>>().Object,
+                   new Mock<IAuthenticationSchemeProvider>().Object)
+        {
+
+        }
+    }
+}
---
+++ BookMeMobi2.Tests/UserServiceTestFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+
+namespace BookMeMobi2.Tests
+{
+    class UserServiceTestFixture<TStarup> : IDisposable where TStarup : class
+    {
+        public readonly TestServer Server;
+        private readonly HttpClient _client;
+
+        public UserServiceTestFixture()
+        {
+            var builder = new WebHostBuilder()
+                .UseContentRoot($"C:\\Users\\msciborski\\Downloads\\source\\repos\\BookMeMobi2\\BookMeMobi2")
+                .UseStartup<TStarup>();
+        }
+        public void Dispose()
+        {
+            Server.Dispose();
+            _client.Dispose();
+        }
+    }
+}
