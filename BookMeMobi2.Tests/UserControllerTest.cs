cf8e5c57ff30ef64a9ab960f2371099fe92fccfc
msciborski <michal.sciborski@student.put.poznan.pl>
2018-04-04 19:40:49 +0200

    Unit test for GetUser (UsersController)

--- BookMeMobi2.Tests/UserControllerTest.cs
+++ BookMeMobi2.Tests/UserControllerTest.cs
@@ -182,6 +182,77 @@ namespace BookMeMobi2.Tests
         }
 
         #endregion
+
+        #region GetUser (Controller)
+
+        [Fact]
+        public async Task GetUserSuccess()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(m => m.GetUser(It.IsAny<string>()))
+                .ReturnsAsync(new User() {Id = "ID1", UserName = "Test1"});
+
+            var usersController = new UsersController(userServiceMock.Object, _logger, _mapper);
+
+            //Act
+            IActionResult actionResult = await usersController.GetUser("ID1");
+
+            //Assert
+            actionResult.ShouldNotBeNull();
+
+            OkObjectResult result = actionResult as OkObjectResult;
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldEqual(200);
+
+            UserDto resultValue = result.Value as UserDto;
+            resultValue.ShouldNotBeNull();
+            resultValue.Id.ShouldEqual("ID1");
+        }
+
+        [Fact]
+        public async Task GeUserInvalidUserId()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(m => m.GetUser(It.IsAny<string>()))
+                .Throws(new UserNoFoundException());
+
+            var usersController = new UsersController(userServiceMock.Object, _logger, _mapper);
+
+            //Act
+            IActionResult actionResult = await usersController.GetUser("ID1");
+
+            //Assert
+            actionResult.ShouldNotBeNull();
+
+            NotFoundObjectResult result = actionResult as NotFoundObjectResult;
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldEqual(404);
+        }
+
+        [Fact]
+        public async Task GeUserInternalError()
+        {
+            //Arrange
+            var userServiceMock = new Mock<IUserService>();
+            userServiceMock.Setup(m => m.GetUser(It.IsAny<string>()))
+                .Throws(new Exception());
+
+            var usersController = new UsersController(userServiceMock.Object, _logger, _mapper);
+
+            //Act
+            IActionResult actionResult = await usersController.GetUser("ID1");
+
+            //Assert
+            actionResult.ShouldNotBeNull();
+
+            JsonResult result = actionResult as JsonResult;
+            result.ShouldNotBeNull();
+            result.StatusCode.ShouldEqual(500);
+        }
+
+        #endregion
     }
 }
 
