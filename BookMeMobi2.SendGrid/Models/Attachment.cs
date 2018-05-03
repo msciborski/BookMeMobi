0da69176967f62d5f64e348933fe07e5c97a3e7d
msciborski <michal.sciborski@student.put.poznan.pl>
2018-05-03 20:07:13 +0200

    Creating models for SendGrid

---
+++ BookMeMobi2.SendGrid/Models/Attachment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookMeMobi2.SendGrid.Models
+{
+    public class Attachment
+    {
+        public string Content { get; set; }
+        public string Type { get; set; }
+        public string FileName { get; set; }
+        public string Disposition { get; set; }
+        public string ContentId { get; set; }
+
+        internal Attachment() { }
+    }
+
+}
---
+++ BookMeMobi2.SendGrid/Models/Personalization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookMeMobi2.SendGrid.Models
+{
+    public class Personalization
+    {
+        public List<EmailAddress> To { get; set; }
+        public string Subject { get; set; }
+
+        internal Personalization() { }
+    }
+}
