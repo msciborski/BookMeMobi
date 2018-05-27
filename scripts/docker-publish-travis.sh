e6ab1dc51dadca577da32eabd0a8a15b5f420ae8
msciborski <michal.sciborski@student.put.poznan.pl>
2018-05-27 19:05:35 +0200

    Adding region confiugarion arn for task in cluster

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -1,9 +1,10 @@
 pip install --user awscli
 export PATH=$PATH:$HOME/.local/bin
+aws configure set region eu-central-1
 $(aws ecr get-login --no-include-email --region eu-central-1)
 docker build -t bookmemobi2 .
 docker ps
 docker tag bookmemobi2:latest 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
 docker push 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
-aws ecs stop-task --cluster bookmemobi-cluster
+aws ecs stop-task --cluster bookmemobi-cluster --task $(aws ecs list-task --cluster bookmemobi-cluster --service bookmemobi-service --output text --query taskArns[0])
 aws ecs run-task --cluster bookmemobi-cluster --task-definition bookmemobi2-task-definition:2
