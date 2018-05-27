b7faf39c1ca2e296272676747ee5bed5ce9d623d
msciborski <michal.sciborski@student.put.poznan.pl>
2018-05-27 13:35:51 +0200

    Docker build in script

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -1,6 +1,2 @@
-pip install --user awscli
-eval $(aws ecr get-login --no-include-email --region eu-central-1)
 docker build -t bookmemobi2 .
 docker ps
-docker tag bookmemobi2:latest 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
-docker push 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
