00e05cb5df030981306134310b3656d370a5a4b0
msciborski <michal.sciborski@student.put.poznan.pl>
2018-05-27 12:05:07 +0200

    Checking if deleting aws task-stop and task-run fixed a problem with parsing travis.yml

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -5,5 +5,3 @@ docker build -t bookmemobi2 .
 docker ps
 docker tag bookmemobi2:latest 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
 docker push 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
-aws ecs stop-task --cluster bookmemobi-cluster
-aws ecs run-task --cluster bookmemobi-cluster --task-definition bookmemobi2-task-definition:2
