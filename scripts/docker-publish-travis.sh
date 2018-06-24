d62a22859866ca75c60f38e97e1631182d7b108e
msciborski <michal.sciborski@student.put.poznan.pl>
2018-06-24 14:32:34 +0200

    Changes task definition number from 2 to 3.

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -4,7 +4,7 @@ aws configure set region eu-central-1
 $(aws ecr get-login --no-include-email --region eu-central-1)
 docker build -t bookmemobi2 .
 docker ps
-docker tag bookmemobi2:latest 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi3:latest
+docker tag bookmemobi2:latest 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
 docker push 601510060817.dkr.ecr.eu-central-1.amazonaws.com/bookmemobi2:latest
 aws ecs stop-task --cluster bookmemobi-cluster --task $(aws ecs list-tasks --cluster bookmemobi-cluster --output text --query taskArns[0])
-aws ecs run-task --cluster bookmemobi-cluster --task-definition bookmemobi2-task-definition:2
+aws ecs run-task --cluster bookmemobi-cluster --task-definition bookmemobi2-task-definition:3
