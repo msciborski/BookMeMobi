a6a5923eb487ec143f20ea746a2ca594f565dd57
msciborski <michal.sciborski@student.put.poznan.pl>
2018-07-07 20:49:22 +0200

    Crearting base resource parameters class, Creating TagResourceParameters class

--- BookMeMobi2/Models/Book/BooksResourceParameters.cs
+++ BookMeMobi2/Models/Book/BooksResourceParameters.cs
@@ -6,20 +6,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BookMeMobi2.Models.Book
 {
-    public class BooksResourceParameters
+    public class BooksResourceParameters : ResourceParametersBase
     {
-        private const int maxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > maxPageSize) ? maxPageSize : value;
-        }
 
+        public BooksResourceParameters()
+        {
+          base.OrderBy = "UploadDate desc";
+        }
         public bool Deleted { get; set; } = false;
         public string SearchQuery { get; set; }
-        public string OrderBy { get; set; } = "UploadDate desc";
         public bool? SentKindle { get; set; } = null;
 
     }
