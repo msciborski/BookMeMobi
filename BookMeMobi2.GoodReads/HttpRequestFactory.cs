1bb735f89ae3ed5fccceed276cb1cb1ebd8ca784
msciborski <michal.sciborski@student.put.poznan.pl>
2018-08-19 12:46:40 +0200

    Creating request factory

---
+++ BookMeMobi2.GoodReads/HttpRequestFactory.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BookMeMobi2.GoodReads
+{
+    public class HttpRequestFactory
+    {
+        public static async Task<HttpResponseMessage> Get(string requestUri, string acceptHeader = "application/json")
+        {
+          var builder = new HttpRequestBuilder().AddMethod(HttpMethod.Get).AddAcceptHeader(acceptHeader).AddRequestUri(requestUri);
+          return await builder.SendAsync();
+        }
+    }
+}
